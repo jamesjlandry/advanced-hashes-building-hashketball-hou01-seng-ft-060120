dfb0f48dc5b0d00234cbc7396eab2501f795558b
James Landry <james.j.landry@outlook.com>
2020-05-25 19:08:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,21 @@
 # Write your code here!
 def game_hash
   hasketball = {
-    home: {team_name: "Brooklyn Nets", colors: "Black, White", players: [{player_name: "", number: 4, shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks: }]
+    home: {team_name: "Brooklyn Nets", colors: "Black, White", players: [{
+      player_name: "", number: 4, shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks: },
+      {player_name: "", number: 4, shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks: }, 
+      {player_name: "", number: 4, shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks: }, 
+      {player_name: "", number: 4, shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks: }, 
+      {player_name: "", number: 4, shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks: }]
     },
-    away: {team_name: "Charlotte Hornets", colors: "Turquise, Purple", players: [{player_name: "", number: 4, shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks: }]}
+    away: {team_name: "Charlotte Hornets", colors: "Turquise, Purple", players: [
+      {player_name: "", number: 4, shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks: },
+      {player_name: "", number: 4, shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks: }, 
+      {player_name: "", number: 4, shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks: },
+      {player_name: "", number: 4, shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks: }, 
+      {player_name: "", number: 4, shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks: }
+      ]
+    }
     
   }
 
