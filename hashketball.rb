b76bf1739a717c1d64f3db59db582ce5e8eeb014
James Landry <james.j.landry@outlook.com>
2020-05-25 19:04:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 # Write your code here!
 def game_hash
   hasketball = {
-    home: {team_name: "Brooklyn Nets", colors: "Black, White", players: [{player_name: "", :number}]
+    home: {team_name: "Brooklyn Nets", colors: "Black, White", players: [{player_name: "", :number 4}]
     },
     away: {team_name: "Charlotte Hornets", colors: "Turquise, Purple", players: []}
     
